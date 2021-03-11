c2430e4361ac1dc0618e1e1a7a4343f2cfbf970e
kasper andersen <kga@live.dk>
2021-03-11 01:58:22 +0100

    costume and initial details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,120 @@
+import 'package:digtial_costume_platform/Costume.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: CostumeDetail(),
+    ));
+
+class CostumeDetail extends StatefulWidget {
+  @override
+  _CostumeDetailState createState() => _CostumeDetailState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+class _CostumeDetailState extends State<CostumeDetail> {
+  IconData fashionToIcon(Fashion fashion) {
+    switch (fashion) {
+      case Fashion.Mens:
+        return Icons.accessibility_outlined;
+      case Fashion.Womens:
+        return Icons.accessible_forward_sharp;
+    }
+  }
+
+  Widget textTemplate(String text) => Text('$text, ');
+
+  Widget costumeTempate(costume) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Column(
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Text('${costume.id},'),
+            Text('${costume.timeperiod},'),
+            Text('${costume.quantity.toString()},'),
+            SizedBox(width: 50.0),
+            Icon(fashionToIcon(costume.fashion))
+          ]),
+          Column(
+            children: <Widget>[
+              Row(children: [
+                Text(
+                  'Themes: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: costume.themes
+                      .map<Widget>((theme) => textTemplate(theme))
+                      .toList(),
+                ),
+              ]),
+              Row(children: [
+                Text(
+                  'Colors: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: costume.colors
+                      .map<Widget>((color) => textTemplate(color))
+                      .toList(),
+                ),
+              ]),
+            ],
+          ),
+          Row(
+            children: [
+              IconButton(icon: Icon(Icons.add), onPressed: null),
+              IconButton(icon: Icon(Icons.edit), onPressed: null),
+              IconButton(icon: Icon(Icons.delete_outline), onPressed: null),
+              FlatButton(onPressed: null, child: Text('Check out'))
+            ],
+          ),
+          Column(children: [
+            Text('TopTen Productions: '),
+            Column(
+              children: costume.productions
+                  .map<Widget>((production) => Text(production.title))
+                  .toList(),
+            ),
+          ])
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<Costume> costumes = [
+    Costume(
+      id: "0",
+      fashion: Fashion.Mens,
+      timeperiod: "1920",
+      category: "Shirt",
+      quantity: 1,
+      colors: ["blue", "green"],
+      themes: [],
+      productions: [
+        Production(
+            title: "first", startDate: DateTime.now(), endDate: DateTime.now()),
+        Production(
+            title: "second",
+            startDate: DateTime.now(),
+            endDate: DateTime.now()),
+        Production(
+            title: "third", startDate: DateTime.now(), endDate: DateTime.now())
+      ],
+      storageLocation: "Lille garderobe - 13",
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Costume Details"),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: costumes.map((costume) => costumeTempate(costume)).toList(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
