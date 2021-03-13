ee192a3955af4a7c0d40e44cef4328620bf088ef
kasper andersen <kga@live.dk>
2021-03-13 21:03:34 +0100

    added localization for english and danish languages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:digtial_costume_platform/Costume.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() => runApp(MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: CostumeDetail(),
     ));
 
@@ -18,9 +21,12 @@ class _CostumeDetailState extends State<CostumeDetail> {
         return Icons.accessibility_outlined;
       case Fashion.Womens:
         return Icons.accessible_forward_sharp;
+      default:
+        return null;
     }
   }
 
+  //wrapper function for creating Text list
   Widget textTemplate(String text) => Text('$text, ');
 
   Widget costumeTempate(costume) {
@@ -28,6 +34,7 @@ class _CostumeDetailState extends State<CostumeDetail> {
       margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
       child: Column(
         children: <Widget>[
+          //Banner of id, time period, quantity and gender marker
           Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
             Text('${costume.id},'),
             Text('${costume.timeperiod},'),
@@ -62,11 +69,14 @@ class _CostumeDetailState extends State<CostumeDetail> {
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              SizedBox(width: 20.0),
               IconButton(icon: Icon(Icons.add), onPressed: null),
               IconButton(icon: Icon(Icons.edit), onPressed: null),
               IconButton(icon: Icon(Icons.delete_outline), onPressed: null),
-              FlatButton(onPressed: null, child: Text('Check out'))
+              FlatButton(onPressed: null, child: Text('Check out')),
+              SizedBox(width: 20.0),
             ],
           ),
           Column(children: [
@@ -90,7 +100,7 @@ class _CostumeDetailState extends State<CostumeDetail> {
       category: "Shirt",
       quantity: 1,
       colors: ["blue", "green"],
-      themes: [],
+      themes: ['first theme', 'second theme', 'third theme', 'fourth', 'fifth'],
       productions: [
         Production(
             title: "first", startDate: DateTime.now(), endDate: DateTime.now()),
@@ -109,12 +119,12 @@ class _CostumeDetailState extends State<CostumeDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Costume Details"),
+        title: Text(AppLocalizations.of(context).helloWorld),
         centerTitle: true,
       ),
       body: Column(
-        children: costumes.map((costume) => costumeTempate(costume)).toList(),
-      ),
+          children:
+              costumes.map((costume) => costumeTempate(costume)).toList()),
     );
   }
 }
